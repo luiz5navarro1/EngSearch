e9dec1b2c186df837d7862324db3dc6fb6e84a1a
Luiz Navarro <luiz5navarro1@gmail.com>
2021-08-15 23:59:14 -0300

    Q2 concluded

--- Q2-FooBaa/foobaa.cpp
+++ Q2-FooBaa/foobaa.cpp
@@ -7,29 +7,9 @@ Program: FooBaa
 int main() {
     /* loop (n)umber from 1 until 100 */
     for (int n = 1; n <= 100; n++) {
-        /* is n multiple of 3? */
-        if (n % 3 == 0) {
-            /* n is multiple of 3 */
-            /* is n multiple of 5 too? */
-            if (n % 5 == 0) {
-                /* n is multiple of 3 and 5 */
-                printf("FooBaa");
-            }
-            else {
-                /* n is just multiple of 3 */
-                printf("Foo");
-            }
-        }
-        /* is n multiple of 5? */
-        else if (n % 5 == 0) {
-            /* n is multiple of 5 */
-            printf("Baa");
-        }
-        /* n not multiple of 3 or 5 */
-        else {
-            printf("%d", n);
-        }
-
+        /* n multiple of 3? n multiple of 3 and 5? n multiple of 5? */
+        (n % 3 == 0) ? ((n % 5 == 0) ? printf("FooBaa") : printf("Foo")) : (n % 5 == 0) ? printf("Baa") : printf("%d", n);
+        /* next line */
         printf("\n");
     }
 
