19a1b05dd8067d1a356971d3171a6e18dd83cc3e
Luiz Navarro <luiz5navarro1@gmail.com>
2021-08-16 00:11:21 -0300

    Q2 concluded

--- Q2-FooBaa/foobaa.cpp
+++ Q2-FooBaa/foobaa.cpp
@@ -8,9 +8,7 @@ int main() {
     /* loop (n)umber from 1 until 100 */
     for (int n = 1; n <= 100; n++) {
         /* n multiple of 3? n multiple of 3 and 5? n multiple of 5? */
-        (n % 3 == 0) ? ((n % 5 == 0) ? printf("FooBaa") : printf("Foo")) : (n % 5 == 0) ? printf("Baa") : printf("%d", n);
-        /* next line */
-        printf("\n");
+        (n % 3 == 0) ? ((n % 5 == 0) ? printf("FooBaa\n") : printf("Foo\n")) : (n % 5 == 0) ? printf("Baa\n") : printf("%d\n", n);
     }
 
     return 0;
